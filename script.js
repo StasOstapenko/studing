efbd295354a68fcf67e9de11c0d0f0d4ab051ba5
StasOstapenko <36300697+StasOstapenko@users.noreply.github.com>
2021-04-06 09:07:34 +0300

    v1

---
+++ script.js
@@ -0,0 +1,70 @@
+var tabulate = function (data,columns) {
+  var table = d3.select('#table1')
+	var thead = table.append('thead')
+	var tbody = table.append('tbody')
+    
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data.csv',function (data) {
+	var columns = ['Програма/проєкт','Період','"Громада з нуля"','"Навчання з надання послуг і управління інфраструктурою на місцевому рівні"', '"Навчання загального управлінського характеру"', 'Кількість учасників навчання (осіб)', 'Цільові групи учасників навчання ', 'Кількість тергромад, для яких було проведене навчання', 'Назва навчання']
+  tabulate(data,columns)
+})
+
+var tabulate2 = function (data,columns) {
+    var table = d3.select('#table2')
+
+	var thead = table.append('thead')
+	var tbody = table.append('tbody')
+    
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text(function (d) { return d })
+
+	var rows = tbody.selectAll('tr')
+	    .data(data)
+	    .enter()
+	  .append('tr')
+
+	var cells = rows.selectAll('td')
+	    .data(function(row) {
+	    	return columns.map(function (column) {
+	    		return { column: column, value: row[column] }
+	      })
+      })
+      .enter()
+    .append('td')
+      .text(function (d) { return d.value })
+
+  return table;
+}
+
+d3.csv('data2.csv',function (data) {
+	var columns = ['Область','Тергромада','Програма', 'Тематика', 'Кількість']
+  tabulate2(data,columns)
+})
